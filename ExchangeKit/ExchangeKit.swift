7ef7eabc5ecf3980aea4b46851439aa0b9242021
Jannic <jannic.marcon@hotmail.com>
2025-05-16 16:05:58 +0200

    Initial Commit

---
+++ ExchangeKit/ExchangeKit.swift
@@ -0,0 +1,9 @@
+//
+//  ExchangeKit.swift
+//  ExchangeKit
+//
+//  Created by Jannic Marcon on 16.05.2025.
+//
+
+import Foundation
+
---
+++ ExchangeKitTests/ExchangeKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeKitTests.swift
+//  ExchangeKitTests
+//
+//  Created by Jannic Marcon on 16.05.2025.
+//
+
+import Testing
+@testable import ExchangeKit
+
+struct ExchangeKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
